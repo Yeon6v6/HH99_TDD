714a5e800d3f82a54b95dc0a63d608f5bdb6f249
yeon <merleawe012@gmail.com>
2024-12-15 15:18:16 +0900

    feat : Initial Project

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,7 +1,11 @@
-package io.hhplus.tdd;
+package io.hhplus.tdd.point;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class PointControllerTest {
+
 }
